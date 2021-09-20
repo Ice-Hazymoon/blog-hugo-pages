c4b7a679569388fb5494cf4ade72d695d129026b
Ice-Hazymoon <imiku.me@gmail.com>
2021-09-20 10:02:57 +0000

    deploy: Ice-Hazymoon/blog-hugo@2975a92386b435476b475e4da5d33a15ba293c82

--- sw.js
+++ sw.js
@@ -1,157 +1,50 @@
-const workboxVersion = '6.3.0';
+const CACHE_NAME = 'hazymoon_sw';
+const OFFLINE_URL = '/offline.html';
+const OFFLINE_IMAGE = '/offline.jpg';
 
-importScripts(`https://storage.googleapis.com/workbox-cdn/releases/${workboxVersion}/workbox-sw.js`);
-workbox.setConfig({
-    debug: false
-});
-
-const CACHE_NAME = 'offline-html';
-const FALLBACK_HTML_URL = '/offline.html';
-self.addEventListener('install', async (event) => {
+self.addEventListener('install', (event) => {
     event.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => cache.add(new Request(FALLBACK_HTML_URL, {
-            cache: "reload"
-        })))
+        caches.open(CACHE_NAME).then((cache) => {
+            cache.add(new Request(OFFLINE_URL, {
+                cache: "reload"
+            }))
+            cache.add(new Request(OFFLINE_IMAGE, {
+                cache: "reload"
+            }))
+        })
     );
 });
-workbox.navigationPreload.enable();
-  // Register this strategy to handle all navigations.
-workbox.routing.registerRoute(
-    new workbox.routing.NavigationRoute(async (params) => {
-        try {
-          // Attempt a network request.
-            return await (new workbox.strategies.NetworkFirst()).handle(params);
-        } catch (error) {
-          // If it fails, return the cached HTML.
-            return caches.match(FALLBACK_HTML_URL, {
-                cacheName: CACHE_NAME,
-            });
-        }
-    })
-);
 
-workbox.core.setCacheNameDetails({
-    prefix: "hazymoon",
-    suffix: "v1",
-    precache:'precache',
-    runtime:'runtime'
+self.addEventListener('activate', (event) => {
+    event.waitUntil((async () => {
+        if ('navigationPreload' in self.registration) {
+            await self.registration.navigationPreload.enable();
+        }
+    })());
+    self.clients.claim();
 });
 
-workbox.core.skipWaiting();
+self.skipWaiting();
 
-workbox.core.clientsClaim();
-
-workbox.precaching.precacheAndRoute([]);
-
-workbox.precaching.cleanupOutdatedCaches();
-
-workbox.routing.registerRoute(
-    ({
-        request
-    }) => request.mode === 'navigate',
-    new workbox.strategies.NetworkFirst({
-        cacheName: 'pages',
-        plugins: [
-            new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [200],
-            }),
-        ],
-    }),
-);
-
-// Images
-workbox.routing.registerRoute(
-    /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
-    new workbox.strategies.CacheFirst({
-        cacheName: "images",
-        plugins: [
-            new workbox.expiration.ExpirationPlugin({
-                maxEntries: 1000,
-                maxAgeSeconds: 60 * 60 * 24 * 30
-            }),
-            new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [0, 200]
-            })
-        ]
-    })
-);
-
-// Fonts
-workbox.routing.registerRoute(
-    /\.(?:eot|ttf|woff|woff2)$/,
-    new workbox.strategies.CacheFirst({
-        cacheName: "fonts",
-        plugins: [
-            new workbox.expiration.ExpirationPlugin({
-                maxEntries: 1000,
-                maxAgeSeconds: 60 * 60 * 24 * 30
-            }),
-            new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [0, 200]
-            })
-        ]
-    })
-);
-
-// Static Libraries
-workbox.routing.registerRoute(
-    /^https:\/\/cdn\.jsdelivr\.net/,
-    new workbox.strategies.CacheFirst({
-        cacheName: "static-libs",
-        plugins: [
-            new workbox.expiration.ExpirationPlugin({
-                maxEntries: 1000,
-                maxAgeSeconds: 60 * 60 * 24 * 30
-            }),
-            new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [0, 200]
-            })
-        ]
-    })
-);
-
-workbox.routing.registerRoute(
-    // Check to see if the request's destination is style for stylesheets, script for JavaScript, or worker for web worker
-    ({
-        request
-    }) =>
-    request.destination === 'style' ||
-    request.destination === 'script' ||
-    request.destination === 'worker',
-    // Use a Stale While Revalidate caching strategy
-    new workbox.strategies.StaleWhileRevalidate({
-        // Put all cached files in a cache named 'assets'
-        cacheName: 'assets',
-        plugins: [
-            // Ensure that only requests that result in a 200 status are cached
-            new workbox.cacheableResponse.CacheableResponse({
-                statuses: [200],
-            }),
-        ],
-    }),
-);
-
-workbox.routing.registerRoute(
-    // Check to see if the request's destination is style for an image
-    ({
-        request
-    }) => request.destination === 'image',
-    // Use a Cache First caching strategy
-    new workbox.strategies.CacheFirst({
-        // Put all cached files in a cache named 'images'
-        cacheName: 'images',
-        plugins: [
-            // Don't cache more than 50 items, and expire them after 30 days
-            new workbox.expiration.ExpirationPlugin({
-                maxEntries: 50,
-                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
-            }),
-            // Ensure that only requests that result in a 200 status are cached
-            new workbox.cacheableResponse.CacheableResponsePlugin({
-                statuses: [200],
-            }),
-        ],
-    }),
-);
-
-workbox.googleAnalytics.initialize();
+self.addEventListener('fetch', (event) => {
+    if (event.request.url.startsWith(self.location.origin) || event.request.url.match(/^https:\/\/cdn\.jsdelivr\.net/)) {
+        // const isSwup = event.request.headers.get('X-Requested-With') === 'swup'
+        event.respondWith((async () => {
+            const cache = await caches.open(CACHE_NAME);
+            try {
+                const networkResponse = await fetch(event.request);
+                if (networkResponse.status === 200) {
+                    await cache.put(event.request, networkResponse.clone());
+                }
+                return networkResponse;
+            } catch (error) {
+                let cachedResponse = await cache.match(event.request);
+                if (cachedResponse) {
+                    return cachedResponse;
+                } else {
+                    return await cache.match(OFFLINE_URL);
+                }
+            }
+        })());
+    }
+});
---
+++ sw_workbox.js
@@ -0,0 +1,154 @@
+const workboxVersion = '6.3.0';
+
+importScripts(`https://storage.googleapis.com/workbox-cdn/releases/${workboxVersion}/workbox-sw.js`);
+workbox.setConfig({
+    debug: false
+});
+
+const CACHE_NAME = 'offline-html';
+const FALLBACK_HTML_URL = '/offline.html';
+self.addEventListener('install', async (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => cache.add(new Request(FALLBACK_HTML_URL, {
+            cache: "reload"
+        })))
+    );
+});
+workbox.navigationPreload.enable();
+workbox.routing.registerRoute(
+    new workbox.routing.NavigationRoute(async (params) => {
+        try {
+            return await (new workbox.strategies.NetworkFirst()).handle(params);
+        } catch (error) {
+            return caches.match(FALLBACK_HTML_URL, {
+                cacheName: CACHE_NAME,
+            });
+        }
+    })
+);
+
+workbox.core.setCacheNameDetails({
+    prefix: "hazymoon",
+    suffix: "v1",
+    precache:'precache',
+    runtime:'runtime'
+});
+
+workbox.core.skipWaiting();
+
+workbox.core.clientsClaim();
+
+workbox.precaching.precacheAndRoute([]);
+
+workbox.precaching.cleanupOutdatedCaches();
+
+workbox.routing.registerRoute(
+    ({
+        request
+    }) => request.mode === 'navigate',
+    new workbox.strategies.NetworkFirst({
+        cacheName: 'pages',
+        plugins: [
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [200],
+            }),
+        ],
+    }),
+);
+
+// Images
+workbox.routing.registerRoute(
+    /\.(?:png|jpg|jpeg|gif|bmp|webp|svg|ico)$/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "images",
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            }),
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    })
+);
+
+// Fonts
+workbox.routing.registerRoute(
+    /\.(?:eot|ttf|woff|woff2)$/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "fonts",
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            }),
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    })
+);
+
+// Static Libraries
+workbox.routing.registerRoute(
+    /^https:\/\/cdn\.jsdelivr\.net/,
+    new workbox.strategies.CacheFirst({
+        cacheName: "static-libs",
+        plugins: [
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 1000,
+                maxAgeSeconds: 60 * 60 * 24 * 30
+            }),
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [0, 200]
+            })
+        ]
+    })
+);
+
+workbox.routing.registerRoute(
+    // Check to see if the request's destination is style for stylesheets, script for JavaScript, or worker for web worker
+    ({
+        request
+    }) =>
+    request.destination === 'style' ||
+    request.destination === 'script' ||
+    request.destination === 'worker',
+    // Use a Stale While Revalidate caching strategy
+    new workbox.strategies.StaleWhileRevalidate({
+        // Put all cached files in a cache named 'assets'
+        cacheName: 'assets',
+        plugins: [
+            // Ensure that only requests that result in a 200 status are cached
+            new workbox.cacheableResponse.CacheableResponse({
+                statuses: [200],
+            }),
+        ],
+    }),
+);
+
+workbox.routing.registerRoute(
+    // Check to see if the request's destination is style for an image
+    ({
+        request
+    }) => request.destination === 'image',
+    // Use a Cache First caching strategy
+    new workbox.strategies.CacheFirst({
+        // Put all cached files in a cache named 'images'
+        cacheName: 'images',
+        plugins: [
+            // Don't cache more than 50 items, and expire them after 30 days
+            new workbox.expiration.ExpirationPlugin({
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 Days
+            }),
+            // Ensure that only requests that result in a 200 status are cached
+            new workbox.cacheableResponse.CacheableResponsePlugin({
+                statuses: [200],
+            }),
+        ],
+    }),
+);
+
+workbox.googleAnalytics.initialize();
